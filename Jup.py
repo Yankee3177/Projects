e17d33bd2022c50287ef8c04de4c767b8206c2a5
Yankee <yankee3177@gmail.com>
2022-03-28 22:23:05 -0400

    For the MySql project, I was able to complete the first part of the project

---
+++ Jup.py
@@ -0,0 +1,5 @@
+name = input("Enter your First Name: ")
+lastName = input("Enter your Last Name: ")
+mayor = input("Enter your Mayor: ")
+
+print("\nHi",name,lastName,",",mayor,"is a really nice mayor!")
---
+++ MySql Project.py
@@ -0,0 +1,75 @@
+import mysql.connector as mysql
+
+def insertQ(tableName):
+    return "INSERT INTO " + tableName + " VALUES (%s, %s)"
+
+def selectQ(columnName,tableName):
+    return "Select " + columnName + " from " + tableName
+
+def delQ(tableName, command):
+    return "DELETE FROM " + tableName + " where " + command
+
+db = mysql.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "03232001",
+    database = "abclass"
+)
+
+action = db.cursor()
+# query = "INSERT INTO hobby VALUES (%s, %s)"
+#
+# values = [
+#     ("0001", "Ping Pong"),
+#     ("0002", "Gym"),
+#     ("3275", "Draw"),
+#     ("0007", "Read")
+# ]
+#
+# action.executemany(query, values)
+#
+# value = [
+#     ("0001", "Basketball"),
+#     ("0002", "Volleyball"),
+#     ("0004", "Wrestling"),
+#     ("0007", "Track"),
+#     ("9000","no sport")
+#     ]
+# q = insertQ("SPORTS")
+#
+# action.executemany(q,value)
+# db.commit()
+
+# selectQuery = selectQ("*", "sports")
+# action.execute(selectQuery)
+# selectData = action.fetchall()
+#
+# for rows in selectData:
+#     print(rows)
+#
+# delQuery = delQ("SPORTS","SPORTNAME = 'no sport'")
+# action.execute(delQuery)
+#
+# db.commit()
+
+selectQuery = selectQ("*","sports")
+action.execute(selectQuery)
+data = action.fetchall()
+
+for rows in data:
+    print(rows)
+print()
+
+selectQuery = selectQ("*","hobby")
+action.execute(selectQuery)
+data = action.fetchall()
+
+for rows in data:
+    print(rows)
+
+
+
+
+
+
+
