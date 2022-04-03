fc1805367b654d97a8d8dcf9431f5a6b21d0a9ac
Yankee <yankee3177@gmail.com>
2022-04-02 22:31:21 -0400

    Fixed all the functions

--- MySql Project.py
+++ MySql Project.py
@@ -1,75 +1,86 @@
 import mysql.connector as mysql
 
-def insertQ(tableName):
-    return "INSERT INTO " + tableName + " VALUES (%s, %s)"
 
-def selectQ(columnName,tableName):
-    return "Select " + columnName + " from " + tableName
+def insertQ(tableName, cursor):
+    itemList = insertItems()
+    value = " VALUES (%s"
+    for i in range(1,len(itemList[0])):
+        value += ",%s"
+    value += ")"
+    q = "INSERT INTO " + tableName + value
+    cursor.executemany(q, itemList)
 
-def delQ(tableName, command):
-    return "DELETE FROM " + tableName + " where " + command
 
-db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "03232001",
-    database = "abclass"
-)
+def selectQ(columnName, tableName, cursor, command=None):
+    if command is None:
+        select = "Select " + columnName + " from " + tableName
+    else:
+        select = "Select " + columnName + " from " + tableName + " where " + command
 
+    cursor.execute(select)
+    selectData = cursor.fetchall()
+
+    for selectRow in selectData:
+        print(selectRow)
+
+
+def delQ(tableName, cursor, command):
+    delQuery =  "DELETE FROM " + tableName + " where " + command
+    cursor.execute(delQuery)
+
+
+def databaseConnect(hostt, userName, passWord, databasee=None):
+    if databasee is None:
+        dbase = mysql.connect(
+            host=hostt,
+            user=userName,
+            passwd=passWord,
+        )
+    else:
+        dbase = mysql.connect(
+            host=hostt,
+            user=userName,
+            passwd=passWord,
+            database=databasee
+        )
+    return dbase
+
+
+def insertItems():
+    item = []
+    cont = True
+    while cont:
+        inItem = list(input("Enter items separated by spaces: ").split(" "))
+        item.append(inItem)
+        done = input("Are you done with all the items? Enter yes to finish with all the items. ").lower()
+        if done == "yes":
+            cont = False
+
+    return item
+
+
+db = databaseConnect("localhost", "root", "03232001", "junk")
 action = db.cursor()
-query = "INSERT INTO hobby VALUES (%s, %s)"
-
-values = [
-    ("0001", "Ping Pong"),
-    ("0002", "Gym"),
-    ("3275", "Draw"),
-    ("0007", "Read")
-]
-
-action.executemany(query, values)
-
-value = [
-    ("0001", "Basketball"),
-    ("0002", "Volleyball"),
-    ("0004", "Wrestling"),
-    ("0007", "Track"),
-    ("9000","no sport")
-    ]
-q = insertQ("SPORTS")
-
-action.executemany(q,value)
-db.commit()
-
-selectQuery = selectQ("*", "sports")
-action.execute(selectQuery)
-selectData = action.fetchall()
-
-for rows in selectData:
-    print(rows)
-
-delQuery = delQ("SPORTS","SPORTNAME = 'no sport'")
-action.execute(delQuery)
 
 db.commit()
 
-selectQuery = selectQ("*","sports")
-action.execute(selectQuery)
-data = action.fetchall()
-
-for rows in data:
-    print(rows)
-print()
-
-selectQuery = selectQ("*","hobby")
-action.execute(selectQuery)
-data = action.fetchall()
-
-for rows in data:
-    print(rows)
+# TODO use all the created functions to make the actual commandline. Remember to make it as easy as possible
+#   so a person that has just the basic knowledge of what a database is knows how to do it.
 
 
 
+# action.executemany(query, values)
+#
+# value = [
+#     ("0001", "Basketball"),
+#     ("0002", "Volleyball"),
+#     ("0004", "Wrestling"),
+#     ("0007", "Track"),
+#     ("9000", "no sport")
+# ]
 
+# delQuery = delQ("SPORTS", "SPORTNAME = 'no sport'")
+# action.execute(delQuery)
 
-
+# db.commit()
 
