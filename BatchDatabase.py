c58b781296e38db2352585dd3c7c232cbf18549f
Yankee <yankee3177@gmail.com>
2022-04-07 23:37:19 -0400

    Finished part 4

--- BatchDatabase.py
+++ BatchDatabase.py
@@ -5,10 +5,18 @@ def readFromTextFile(fileName):
     with open(fileName, 'r') as file:
         return file.read().splitlines()
 
-def insert(insertList,cursor,db):
+
+def insert(insertList, cursor, db):
     for i in range(len(insertList)):
-        cursor.executemany("INSERT INTO Students VALUES(%s,%s,%s,%s,%s,%s)", insertList[i])
+        if len(insertList[i]) == 5:
+            insertList[i].append(None)
+
+        q = """INSERT INTO students VALUES (%s, %s, %s, %s, %s, %s)"""
+        cursor.execute(q, insertList[i])
+
         db.commit()
+        print("\nInsert complete")
+
 
 def databaseConnect(hostt, userName, passWord, databasee=None):
     if databasee is None:
@@ -26,6 +34,7 @@ def databaseConnect(hostt, userName, passWord, databasee=None):
         )
     return dbase
 
+
 def idNums(cursor):
     selQuery = "SELECT StudentID FROM Students"
 
@@ -38,7 +47,7 @@ def idNums(cursor):
 
 
 def schoolId(cursor):
-    selQuery = "SELECT SchoolID FROM Schools"
+    selQuery = "SELECT SchoolsID FROM School"
 
     cursor.execute(selQuery)
     selectData = cursor.fetchall()
@@ -58,6 +67,7 @@ def hobbyId(cursor):
         hobbyList.append(int(str(sel).strip("(,)")))
     return hobbyList
 
+
 def sportId(cursor):
     selQuery = "SELECT SportID FROM Sports"
 
@@ -69,104 +79,141 @@ def sportId(cursor):
     return sportList
 
 
-def manipulateFile(fileName,cursor,db):
+def manipulateFile(fileName, cursor, db):
     txtInput = readFromTextFile(fileName)
     insertList = []
     txtID = []
+    errorList = []
     for i in txtInput:
         insertList.append(i.split(" "))
 
     for i in range(len(insertList)):
         if len(insertList[i]) < 5 or len(insertList[i]) > 6:
-            print("This input is not valid")
+            errorList.append("Error: " + str(insertList[i]) + " is not a valid input")
             insertList.remove(insertList[i])
             continue
 
         if insertList[i][2].isnumeric():
             insertList[i][2] = int(insertList[i][2])
             if insertList[i][2] in txtID:
-                print("Error: ID already exists")
+                errorList.append("Error: " + str(insertList[i]) + " student id is duplicated.")
                 insertList.remove(insertList[i])
+                continue
             else:
                 txtID.append(insertList[i][2])
         else:
-            print("Error: " + insertList[i][2] + " is not a number")
+            errorList.append(
+                "Error: " + str(insertList[i]) + " student id is not a number which means the format isn't correct")
             insertList.remove(insertList[i])
+            continue
 
         if insertList[i][3].isnumeric():
             insertList[i][3] = int(insertList[i][3])
         else:
-            print("Error: " + insertList[i][3] + " is not a number")
+            errorList.append(
+                "Error: " + str(insertList[i]) + " school id is not a number which means the format isn't correct")
             insertList.remove(insertList[i])
+            continue
 
         if insertList[i][4].isnumeric():
             insertList[i][4] = int(insertList[i][4])
         else:
-            print("Error: " + insertList[i][4] + " is not a number")
+            errorList.append(
+                "Error: " + str(insertList[i]) + " hobby id is not a number which means the format isn't correct")
             insertList.remove(insertList[i])
+            continue
 
         if len(insertList[i]) == 6:
             if insertList[i][5].isnumeric():
                 insertList[i][5] = int(insertList[i][5])
             else:
-                print("Error: " + insertList[i][5] + " is not a number")
+                errorList.append(
+                    "Error: " + str(insertList[i]) + " sports id is not a number which means the format isn't correct")
                 insertList.remove(insertList[i])
-
 
     studId = idNums(cursor)
     schId = schoolId(cursor)
     hyID = hobbyId(cursor)
     spId = sportId(cursor)
 
-    for i in insertList:
-        if i[2] in studId:
-            print("Error: " + str(i[2]) + " already exists")
-            insertList.remove(i)
+    for i in range(len(insertList)):
+        if insertList[i][2] in studId:
+            errorList.append("Error: " + str(insertList[i][0]) + " student id is duplicated")
+            insertList.remove(insertList[i])
             continue
 
-        if i[3] not in schId:
-            add = input("School ID " + str(i[3]) + " does not exist. Would you like to add it? (y/n) ").lower()
+        if insertList[i][3] not in schId:
+            add = input(
+                "School ID " + str(insertList[i][3]) + " does not exist. Would you like to add it? (y/n) ").lower()
             if add == "y":
-                schoolName = input("Please enter the school name: ")
+                schoolName = input("\nPlease enter the school name: ")
                 schoolCity = input("Please enter the school city: ")
                 schoolState = input("Please enter the school state: ")
 
-                cursor.execute("INSERT INTO School VALUES(%s,%s,%s,%s)", (i[3],schoolName,schoolCity,schoolState))
-                print("School added")
+                cursor.execute("INSERT INTO School VALUES(%s,%s,%s,%s)",
+                               (insertList[i][3], schoolName, schoolCity, schoolState))
+                print("School added\n")
             else:
-                print("School not added")
-                insertList.remove(i)
+                errorList.append(
+                    "Error: " + str(insertList[i][0]) + " school not added, so school does not exist in database.")
+                insertList.remove(insertList[i])
                 continue
 
-        if i[4] not in hyID:
+        if insertList[i][4] not in hyID:
             hyAdd = input("Hobby ID " + str(i[4]) + " does not exist. Would you like to add it? (y/n) ").lower()
             if hyAdd == "y":
-                hobbyName = input("Please enter the hobby name: ")
+                hobbyName = input("\nPlease enter the hobby name: ")
 
-                cursor.execute("INSERT INTO Hobbies VALUES(%s,%s)", (i[4],hobbyName))
-                print("Hobby added")
+                cursor.execute("INSERT INTO Hobbies VALUES(%s,%s)", (insertList[i][4], hobbyName))
+                print("Hobby added\n")
             else:
-                print("Hobby not added")
-                insertList.remove(i)
+                errorList.append(
+                    "Error: " + str(insertList[i][0]) + " hobby not added, so hobby does not exist in database.")
+                insertList.remove(insertList[i])
                 continue
 
-        if len(i) == 6:
-            if i[5] not in spId:
-                spAdd = input("Sport ID " + str(i[5]) + " does not exist. Would you like to add it? (y/n) ").lower()
+        if len(insertList[i]) == 6:
+            if insertList[i][5] not in spId:
+                spAdd = input(
+                    "Sport ID " + str(insertList[i][5]) + " does not exist. Would you like to add it? (y/n) ").lower()
                 if spAdd == "y":
                     sportName = input("Please enter the sport name: ")
 
-                    cursor.execute("INSERT INTO Sports VALUES(%s,%s)", (i[5],sportName))
-                    print("Sport added")
+                    cursor.execute("INSERT INTO Sports VALUES(%s,%s)", (insertList[i][5], sportName))
+                    print("Sport added\n")
                 else:
-                    print("Sport not added")
-                    insertList.remove(i)
-                    continue
+                    errorList.append(
+                        "Error: " + str(insertList[i][0]) + " sport not added, so sport does not exist in database.")
+                    insertList.remove(insertList[i])
+
     db.commit()
-    return insertList
+    return insertList, errorList
 
-dataB = databaseConnect("localhost", "root", "03232001", "abclass")
-action = dataB.cursor()
 
-# TODO - send the error messages to a text file, format the inputs so that they look good on the command line. Make the main func.
+def writeToFile(errorList):
+    file = open("errors.txt", "w")
+    file.write("\n".join(errorList))
+    file.close()
 
+
+def main():
+    host = input("Enter the host name: ")
+    userName = input("Enter the user name: ")
+    passWord = input("Enter the password: ")
+    database = input("Enter the database name: ")
+    print()
+    dataB = databaseConnect(host, userName, passWord, database)
+    action = dataB.cursor()
+
+    file = input("\nEnter name of text file: ")
+    file += ".txt"
+    inStuds, errList = manipulateFile(file, action, dataB)
+    insert(inStuds, action, dataB)
+
+    if len(errList) > 0:
+        writeToFile(errList)
+        print("\nErrors written to errors.txt")
+
+
+main()
+#Format for txt file is firstName lastName studentid schoolid hobbyid sportid
